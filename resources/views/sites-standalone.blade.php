36c067eb674a48235b7b571b1faed9cd11ab36f7
Sonvp21 <nguyensonvp21@gmail.com>
2026-09-07 09:42:47 +0700

    add logo to head

--- resources/views/sites-standalone.blade.php
+++ resources/views/sites-standalone.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
+  <link rel="icon" type="image/jpeg" href="/girc-logo.jpg">
+  <link rel="apple-touch-icon" href="/girc-logo.jpg">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hội thảo Ứng dụng AI & IoT</title>
